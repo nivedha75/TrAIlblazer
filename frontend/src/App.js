3d78aa9d151fadc2b1abaf644671bcf78c8ae897
coderRamL <rlaxmina@purdue.edu>
2025-02-08 01:34:31 -0500

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,21 @@ import React, { useEffect, useState } from 'react';
    function App() {
        const [message, setMessage] = useState('');
        useEffect(() => {
-           fetch('/api/hello') 
-           .then(response => response.json())
-           .then(data => console.log(data))
-           .catch(error => console.error("Error fetching data:", error));
+        fetch("http://127.0.0.1:5000/api/hello")
+        .then((response) => response.json())
+        .then((data) => {
+          if (data && data.message) {
+            setMessage(data.message);
+          } else {
+            throw new Error("Message not found");
+          }
+        })
+        .catch((error) => console.error("Error fetching data:", error));
        }, []);
        return (
            <div>
                <h1>{message}</h1>
+               <button onClick={() => alert("Button Clicked!")}>Click Me</button>
            </div>
        );
    }
