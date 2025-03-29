73dea2eb78d32db805b5462bba68ba09f05d3ec9
edwardkelley <112304320+edwardkelley@users.noreply.github.com>
2025-03-29 16:50:09 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -681,12 +681,12 @@ def generate_restaurant_recommendations(user_id, location, trip_id):
 def get_image(query):
     # Replace these with your actual API key and Custom Search Engine (CX) ID
         # Praveer
-    #API_KEY = "AIzaSyClHKoSP7fOjxrCB2Dx94szQs5fOMjJsx4"
-    #CX = "750536663af024901"
+    API_KEY = "AIzaSyClHKoSP7fOjxrCB2Dx94szQs5fOMjJsx4"
+    CX = "750536663af024901"
     
     # Ed
-    API_KEY = "AIzaSyBEKQ5QQXSMGXSyVQ3UmvqgrvxjKq__g_0"
-    CX = "564958a336c094aae"
+    #API_KEY = "AIzaSyBEKQ5QQXSMGXSyVQ3UmvqgrvxjKq__g_0"
+    #CX = "564958a336c094aae"
 
 
     # Search query
